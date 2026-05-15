877fd22ac02ae6c96e913939a86a5aaf9b288580
umesh-patidar-05 <umesh2005@gmail.com>
2026-05-16 00:23:51 +0530

    15/may/26

--- 019.py
+++ 019.py
@@ -219,7 +219,7 @@ else:
 
 
 
-
+'''
 password=input("Enter the password ")
 upper=0
 lower=0
@@ -248,3 +248,33 @@ if len(password)>=8:
     
 else:
     print("Invalid")    
+'''    
+    
+    
+    
+    
+password=input("Enter the password ")
+upper=0
+lower=0
+digit=0
+space=0
+special=0
+if len(password)>=8:
+    for ch in password:
+        if ch>='A' and ch<='Z':
+            upper=1
+        elif ch>='a' and ch<='z':
+            lower=1
+        elif ch>='0' and ch<='9':
+            digit=1
+        elif ch==" ":
+            space=1
+        else:
+            special=1
+    if upper==1 and lower==1 and digit==1 and space==0 and special==1:
+        print("Valid")
+    else:
+        print("Invalid")
+    
+else:
+    print("Invalid")        
