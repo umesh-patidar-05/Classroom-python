87b01f1928acad45d0359b4b163ed9afb91147d8
UMESH PATIDAR <umesh2005p@gmail.com>
2026-06-28 13:49:51 +0530

    correct\

--- 038_arguments_or_parameters.py
+++ 038_arguments_or_parameters.py
@@ -307,44 +307,6 @@ def main():
     def mul(a,b):
         c=a*b 
         print(c)        
-
-    while True:
-        choice=int(input("Enter first number: "))
-        match choice:
-            case 1:
-                sum(a,b)
-                
-            case 2:
-                sub(a,b)
-              
-            case 3:
-                mul(a,b)     
-            case _:
-                break                               
-main()                
-'''
-
-
-
-
-
-
-'''
-def main():
-    a=int(input("Enter first number: "))
-    b=int(input("Ente second number: "))
-    
-    def sum(a,b):
-        c=a+b
-        print(c)
-        
-    def sub(a,b):
-        c=a-b 
-        print(c)
-
-    def mul(a,b):
-        c=a*b 
-        print(c)        
     print("1. sum\n2.subtract\n3. multiplication\n4. exit")    
 
     while True:
